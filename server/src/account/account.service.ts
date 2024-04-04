eeda8378ed0eb28b5b7e574f2c8ac060ae72043d
NataDent <cherkashyna.n@gmail.com>
2024-04-05 02:10:24 +0300

    Initial commit

---
+++ server/src/account/account.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { PatchAccountDto } from './dto';
+
+@Injectable()
+export class AccountService {
+    constructor( private db: DbService ) { }
+
+    async create( userId: number ) {
+        return this.db.account.create( {
+            data: {
+                ownerId: userId,
+                isBlockingEnabled: false,
+            }
+        } );
+    }
+
+    async getAccount( userId: number ) {
+        return this.db.account.findUniqueOrThrow( { where: { ownerId: userId } } );
+    }
+
+    patchAccount( userId: number, patch: PatchAccountDto ) {
+        return this.db.account.update( {
+            where: { ownerId: userId },
+            data: { ...patch },
+        } );
+    }
+}
